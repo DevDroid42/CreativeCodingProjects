6ffa9c7d833427917506fa977c58f05f4bc47855
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2024-02-23 16:13:03 -0600

    removed shaders

--- build/build.js
+++ build/build.js
@@ -1,5 +1,6 @@
 let balls;
 let dTime = 0;
+let hueOffset = 0;
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 class Ball {
     constructor(position, speed, diameter, color) {
@@ -22,11 +23,11 @@ function tickCircles() {
         fill(ball.color);
         if (ball.position.x > windowWidth - ball.diameter / 2 || ball.position.x < ball.diameter / 2) {
             ball.position.x = clamp(ball.position.x, ball.diameter / 2, windowWidth - ball.diameter / 2);
-            ball.speed.x *= -0.5;
+            ball.speed.x *= -0.3;
         }
         if (ball.position.y > windowHeight - ball.diameter / 2 || ball.position.y - ball.diameter / 2 < 0) {
             ball.position.y = clamp(ball.position.y, ball.diameter / 2, windowHeight - ball.diameter / 2);
-            ball.speed.y *= -0.5;
+            ball.speed.y *= -0.3;
         }
         ball.position.add(p5.Vector.mult(ball.speed, dTime));
         ball.speed.y += 80 * dTime;
@@ -38,7 +39,7 @@ function circleCollision() {
     balls.forEach((ball) => {
         vertexData.forEach((vertex) => {
             if (Math.abs(ball.position.x - vertex.x) < ball.diameter / 4 && ball.position.y > vertex.y + 5) {
-                ball.position.y = vertex.y + 5;
+                ball.position.y = vertex.y + 10;
             }
             else if (ball.position.dist(vertex) < ball.diameter / 2) {
                 let delta = p5.Vector.sub(ball.position, vertex);
@@ -61,19 +62,20 @@ function setup() {
 const spectrumSize = 1024;
 const vertexData = new Array(spectrumSize);
 function draw() {
+    hueOffset += 1 * dTime;
     dTime = deltaTime / 1000;
     fft.smooth(0.9);
-    background(255);
+    background(0);
     tickCircles();
     let spectrum = fft.analyze(spectrumSize);
     for (let i = 0; i < spectrum.length; i++) {
         vertexData[i] = createVector((i / spectrum.length) * windowWidth, map(spectrum[i] * 2, 0, 255, height, 0));
     }
     push();
-    for (let i = 0; i < spectrum.length; i += 2) {
+    for (let i = 0; i < spectrum.length - 1; i++) {
         beginShape(QUADS);
-        stroke((i / spectrum.length) * 128, 255, 255);
-        fill((i / spectrum.length) * 128, 255, 255);
+        stroke((i / spectrum.length) * 128 + hueOffset * 20, 255, 255);
+        fill((i / spectrum.length) * 128 + hueOffset * 20, 255, 255);
         vertex(vertexData[i].x, windowHeight);
         vertex(vertexData[i].x, vertexData[i].y);
         vertex(vertexData[i + 1].x, vertexData[i + 1].y);
