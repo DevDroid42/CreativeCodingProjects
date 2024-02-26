0ef6d29b8839a92cf96f3e96d387a7a80b03bccc
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2024-02-25 22:14:09 -0600

    experimental mono repo

---
+++ Assignment3/build/sketch_copy.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.test = void 0;
+class test {
+    constructor(testParam) {
+        this.testParam = testParam;
+    }
+}
+exports.test = test;
+//# sourceMappingURL=sketch_copy.js.map
