9ee3a0bff428f6c93e18f39b8834d1318b7e0c45
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2024-03-11 21:18:33 -0500

    added noise set

---
+++ Sets/Set2/build/build.js
@@ -0,0 +1,47 @@
+let time = 0;
+const noiseScale = 0.005;
+class Sphere {
+    constructor(position) {
+        this.position = position;
+    }
+    draw() {
+        push();
+        let r = noise(cos(this.position.x / 25) * 0.5 + time, sin(this.position.z / 25) * 0.5 + time) * this.position.y * this.position.y * 0.1;
+        let g = noise(cos(this.position.x / 25) * 0.5 + time + 1, sin(this.position.z / 25) * 0.5 + time + 1) * this.position.y * this.position.y * 0.1;
+        let b = noise(cos(this.position.x / 25) * 0.5 + time + 2, sin(this.position.z / 25) * 0.5 + time + 2) * this.position.y * this.position.y * 0.1;
+        ambientMaterial(r, g, b);
+        translate(this.position);
+        sphere(10, 3, 2);
+        pop();
+    }
+}
+let spheres = [];
+let fft;
+function setup() {
+    frameRate(120);
+    for (let x = 0; x < 60; x++) {
+        for (let z = 0; z < 60; z++) {
+            spheres.push(new Sphere(createVector((x - 30) * 10, 0, (z - 30) * 10)));
+        }
+    }
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    fft = new p5.FFT();
+    let mic = new p5.AudioIn();
+    mic.start();
+    fft.setInput(mic);
+    fft.smooth(0);
+}
+function draw() {
+    fft.analyze();
+    time += deltaTime / 1000 * abs(mouseX - width / 2) / (width / 2);
+    time += fft.getEnergy(50, 10000) / 2000;
+    background(0);
+    noStroke();
+    ambientLight(255);
+    orbitControl();
+    spheres.forEach(s => {
+        s.position.y = noise(s.position.x * noiseScale + time, s.position.z * noiseScale + time) * 100 * (0.5 + fft.getEnergy(50, 10000) / 200);
+        s.draw();
+    });
+}
+//# sourceMappingURL=build.js.map
